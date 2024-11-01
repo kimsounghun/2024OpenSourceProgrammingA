c0641d27e0dc370079625068dbda0c33bab51d1e
kimsounghun <dkdl458056@gmail.com>
2024-11-01 10:36:36 +0900

    v2.2

---
+++ week09/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var result string
+	result = fmt.Sprintf("%0.1f 나누기 %0.1f는 %0.2f입니다\n", 1.0, 3.0, 1.0/3.0)
+	fmt.Print(result)
+
+	i := 1
+	for i <= 10 {
+		fmt.Printf("%d\n", i)
+		i++
+	}
+}
